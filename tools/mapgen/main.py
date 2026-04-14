63fbc6642e73d31d011fad1f2cde96fb0701b415
Siddharth Chopra <contact.sid.chopra@gmail.com>
2026-04-14 20:37:52 +0530

    add script to gen map

---
+++ tools/mapgen/main.py
@@ -0,0 +1,39 @@
+import json
+
+# hvce
+
+j = None
+
+with open('testmap.json') as f:
+    c = f.read()
+    j = json.loads(c)
+    
+layers = j['layers']
+
+buffer = ""
+
+l = layers[0]
+
+width = l['width']
+height = l['height']
+d = l['data']
+for x in range(width):
+    for y in range(height):
+        tmid_raw = d[y*width + x]
+        tmid_b = format(tmid_raw, "032b")
+        flag_h = tmid_b[0]
+        flag_v = tmid_b[1]
+        flag_d = tmid_b[2]
+        b = tmid_b[-14:]
+        
+        o = ""
+        o += b
+        o += flag_h
+        o += flag_v
+        o += '0'
+        o += '0'
+        buffer += o
+                
+print(buffer)
+with open("map.bin", 'w') as f:
+    f.write(buffer)
