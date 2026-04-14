92a01b553c9ecbc4e1a2d433c6220903125cc94e
Siddharth Chopra <contact.sid.chopra@gmail.com>
2026-04-14 23:31:41 +0530

    fix small bug

--- tools/mapgen/mario/mario.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="mario" tilewidth="8" tileheight="8" tilecount="512" columns="16">
- <image source="mario.png" width="128" height="256"/>
-</tileset>
